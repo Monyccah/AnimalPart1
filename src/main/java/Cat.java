c31056ee42e95e58cc449c361a235cfd4640f7c5
Monyccah <59962787+Monyccah@users.noreply.github.com>
2020-02-10 12:36:20 +0200

    Update Cat.java

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,6 +1,6 @@
 public class Cat extends Animal {
 
-    private static String name = "MoMo";
+    private static String name = "Rax";
 
     public void sounds() {
         System.out.println("Cat Meows!!");
